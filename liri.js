d31519c0cca8458216245b87f0e74e96284377e9
bbanderson84 <bbanderson84@gmail.com>
2019-02-28 15:04:15 -0700

    updated copy

--- liri.js
+++ liri.js
@@ -3,77 +3,102 @@ require("dotenv").config();
 
 //variable to store & import keys.js file
 var keys = require("./keys.js");
-
+var request = require("request");
 var fs = require ("fs");
-var Spotify = require("node-spotify-api");
+var spotify = require("node-spotify-api");
 var axios = require("axios");
 
 
+// stored arguments/ input array
 var userInput = process.argv;
 var userAction = userInput[2];
-var secondInput = userInput[3];
 
+// variable to store movie, song, or concert
+var input = "";
+
+//for loop attaching multiple word arguments
+for (var i = 3; i < userInput.length; i++) {
+
+    if (i > 3 && i <userInput.length) {
+
+        input = input + "+" + userInput[i];
+
+    } else {
+
+        input += userInput[i];
+    }
+}
+
+// switch case
 switch (userAction) {
-    case "movie-this":
-    movie(secondInput);
+    case "concert-this":
+    concertThis();
     break;
+
+    case "spotify-this-song":
+    if (input){
+        spotifyThis(input);
+    } else {
+    //     spotifyThis("")
+    }
+    break;
+
+    case "movie-this":
+    if (input){
+        movieThis(input);
+    } else {
+        movieThis("Mr. Nobody")
+    }
+    break;
+
+    case "do-what-it-says":
+    doThisThing();
+    break;
+
+    default:
+        console.log("{Please enter: concert-this, spotify-this-song, movie-this, or do-what-it-says}");
+    break;
+}
+
+
+
+var concertThis = function () {
 
 }
 
-function movie (secondInput) {
-// create empty variable for holding the movie title
-var movieName = "";
+var spotifyThis = function () {
 
-// loop through all of the words in the argument
-//use for loop to handle inclusion of "+"'s
-// for (var i = 2; i < userInput.length; i++) {
+}
 
-//     if (i > 2 && i < userInput.length) {
-//         movieName = movieName + "+" + userInput[i];
+var movieThis = function (movieName) {
 
-//     }
-//     else {
-//         movieName += userInput[i];
-//     }
-// }
+    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-// Run request with axios to OMDB API with movie specified 
-var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-// for debug
-// console.log(queryURL);
-
-// Requesting through axios then logging title, year, rating, rotten tomatoes rating, country, language, plot, actors to terminal.
-request(queryURL, function (error, response, body) {
+// Requesting through axios then logging title, year, rating, rotten tomatoes rating, country, language, plot, actors to terminal.  
+    request(queryURL, function (error, response, body) {
+        
 
     // if (movieName === "") {
 
     //     movieName = 'Mr. Nobody';
     // }
 
-    for (var i = 2; i < userInput.length; i++) {
+  
 
-        if (i > 2 && i < userInput.length) {
-            movieName = movieName + "+" + userInput[i];
-        }
-        else {
-            movieName += userInput[i];
-        }
-    }
+    // if (movieName === "") {
 
-    if (movieName === "") {
-
-        movieName = 'Mr. Nobody';
-    }
+    //     movieName = 'Mr. Nobody';
+    // }
 
         console.log("Title of the movie: " + JSON.parse(body).Title);
-        console.log("Release year: " + response.data.Year);
-        console.log("Movie rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
-        console.log("Country of origin: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Movie plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
+        console.log("Release year: " + JSON.parse(body).Year);
+        console.log("Movie rating: " + JSON.parse(body).imdbRating);
+        // console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country of origin: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Movie plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
 
     }
 )
