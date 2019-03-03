7f65e39fa1e3751ad60312377865a2cbb52bd4c7
bbanderson84 <bbanderson84@gmail.com>
2019-03-03 12:25:40 -0700

    added concert-this and fixed movie-this

--- liri.js
+++ liri.js
@@ -1,26 +1,26 @@
 //read and set any environment variables with the dotenv package:
 require("dotenv").config();
 
-//variable to store & import keys.js file
+//global variables
 var keys = require("./keys.js");
 var request = require("request");
 var fs = require ("fs");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
-
+var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
 
 
 // stored arguments/ input array
 var userInput = process.argv[2];
-var userAction = process.argv[3];
+var userAction = process.argv.slice(3).join(" ");
 runApp();
 
 
-
-
+// created a function including the swith case 
 function runApp() {
-// switch case
+
+// switch case for the user input argument
 switch (userInput) {
     case "concert-this":
         concertThis();
@@ -31,7 +31,6 @@ switch (userInput) {
         break;
 
     case "movie-this":
-        // movieThis(userAction);
         movieThis();
         break;
 
@@ -41,79 +40,84 @@ switch (userInput) {
    }
 }
 
+// concert-this function
+ function concertThis () {
 
-// var concertThis = function () {
+    var queryURL = "https://rest.bandsintown.com/artists/" + userAction + "/events?app_id=codingbootcamp";
 
-// }
+    request(queryURL, function(error, response, body) {
 
+            // if there's an error, console log the error
+            if (error) console.log(error);
 
+            // sets result variable, if everything in search goes right, console logs venue name, location, and date of concert using moment.js
+            var result = JSON.parse(body)[0];
+            if (!error && response.statusCode === 200) {
+                console.log("Venue Name: " + result.venue.name + "\nVenue Location: " + result.venue.city +  "\nDate: " + moment(result.dateTime).format("MM/DD/YYYY"));
+              
+                };
+
+        });
+
+    }    
+       
+// spotify-this function
 function spotifyThis() {
+
+    // if there is an error in user action, or nothing is entered, default search is to "The Sign" by Ace of Bass
     if (!userAction) {
     
         userAction = "the sign by ace of base"
     }
     
+        // spotify searches the song name the user enters, if there is an erorr returns the erro
         spotify.search({ type: 'track', query: userAction }, function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
     
-            // need to code is no song default to ace of base
+            // sets data variable, console los the artist name, song title, preview url, and album name
             var data = data.tracks.items[0];
-            console.log(data.artists[0].name);
-            console.log(data.name);
-            console.log(data.preview_url);
-            console.log(data.album.name);
-            
-
+            console.log("Artist: " + data.artists[0].name + "\nSong: " + data.name +
+            "\nPreview URL: " + data.preview_url + "\nAlbum: " + data.album.name);
+          
         });
     }
 
+// movie-this function
 function movieThis() {
 
-    var input = "";
+    // if there is an error, or user does not enter a movie title, it defaults the search to "Mr. Nobody"
+    if (!userAction){
 
-    for (var i = 2; i < userInput.length; i++) {
-        if ( i > 2 && i < userInput.length){
-            input = input + "+" + userInput[i];
-        } else {
-            input += userInput[i];
-        }
+        userAction = 'Mr. Nobody';
+
     }
-        if (userAction === undefined){
-
-            userAction = 'Mr. Nobody';
-
-            }
 
         var queryURL = "http://www.omdbapi.com/?t=" + userAction + "&y=&plot=short&apikey=trilogy";
         
         request(queryURL, function(error, response, body) {
 
-      
-        if (!error && response.statusCode === 200) {
+    // if search is correct, console logs title, release year, IMDB rating, rotten tomatoes rating, country, language, plot, actors
+    if (!error && response.statusCode === 200) {
     
-                console.log("Title: " + JSON.parse(body).Title);
-                console.log("Release Year: " + JSON.parse(body).Year);
-                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-                console.log("Country: " + JSON.parse(body).Country);
-                console.log("Language: " + JSON.parse(body).Language);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors: " + JSON.parse(body).Actors);
+        console.log("Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).imdbRating + "\nRotten Tomatoes Rating: " + JSON.parse(body).tomatoRating + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors);
+               
             };
         });
     };
-    
-    function doThisThing() {
 
+// do-what-it-says function
+function doThisThing() {
+
+        // reads the information located in "random.txt", console logs error if there is one
         fs.readFile("random.txt", "utf8", function(error, data){
             if(error){
                return  console.log(error)
             }
         data = data.split(",");
-        action = data[0];
-        userChoice = data[1];
+        userInput = data[0];
+        userAction = data[1];
     
         runApp();
         });
